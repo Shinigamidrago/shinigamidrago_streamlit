963778aaa9e485877b6e60e40db7e73fb1478b22
shinigamidrago <brunocontisimon@gmail.com>
2023-07-27 12:26:15 -0300

    corrije error with

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ import streamlit as st
 import numpy as np
 
 
-left_column,cent_colum, rigth_column = st.columns([7, 3])
+
+left_column, rigth_column = st.columns([7, 3])
 
 with left_column:
     chart_data = pd.DataFrame(
@@ -17,30 +18,35 @@ with left_column:
     
 
 with rigth_column:
+    rigth_column.markdown("## ")
     chosen = st.radio(
         'Shorting hat',
         ("Op 1 ", "Op 2", "Op 3", "Op 4"))
     st.write(f"La eleccion es {chosen}")
 
+c = st.checkbox(
+      label="Codigo",
+      value= False
+)
 
-"""with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+if(c == True):
+    st.write( """   "with st.echo(code_location='below'):
+                    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+                    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+                Point = namedtuple('Point', 'x y')
+                data = []
 
-    points_per_turn = total_points / num_turns
+                points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+                for curr_point_num in range(total_points):
+                    curr_turn, i = divmod(curr_point_num, points_per_turn)
+                    angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+                    radius = curr_point_num / total_points
+                    x = radius * math.cos(angle)
+                    y = radius * math.sin(angle)
+                    data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
-"""
+                st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+                .mark_circle(color='#0068c9', opacity=0.5)
+                .encode(x='x:Q', y='y:Q')) """ )
